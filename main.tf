2e8083b3b7008aad7bb4218b896bf5d40c1ac3b2
Lucas de Ataides <lucasatab@gmail.com>
2022-11-10 23:30:42 -0300

    Create terraform file for testing

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 1.3.2"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "cumulus-test" {
+  ami = "ami-09a41e26df464c548"
+  instance_type = "t2.large"
+  key_name = "keypair-lucas-inspiron7572"
+  tags = {
+    Name = "Cumulus-Testing"
+  }
+}
